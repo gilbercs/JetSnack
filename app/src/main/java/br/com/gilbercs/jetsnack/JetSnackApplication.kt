01a1b37d3020c118aac54049fab1d3cf3947f0c2
Gilbercs <gilbercs@hotmail.com>
2023-05-27 22:46:56 -0400

    create class hiltAndroidApp e adicionado no manisfet

--- app/src/main/java/br/com/gilbercs/jetsnack/JetSnackApplication.kt
+++ app/src/main/java/br/com/gilbercs/jetsnack/JetSnackApplication.kt
@@ -1,10 +1,11 @@
 package br.com.gilbercs.jetsnack
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import dagger.hilt.android.lifecycle.HiltViewModel
 //------------------------------------------------
 //Criado e adicionado no manifest a class com anotação
 //Hilt para gerencia o ciclo de vida
 //------------------------------------------------
-@HiltViewModel
+@HiltAndroidApp
 class JetSnackApplication: Application()
