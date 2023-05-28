dd788f29f99b02fb191c554123d090740f1d132c
Gilbercs <gilbercs@hotmail.com>
2023-05-27 21:56:36 -0400

    create os pacotes implementar a arquitetura do App, comentário sobre o App na Main

--- app/src/main/java/br/com/gilbercs/jetsnack/MainActivity.kt
+++ app/src/main/java/br/com/gilbercs/jetsnack/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.gilbercs.jetsnack.ui.theme.JetSnackTheme
-
+//----------------------------------------------------
+//Applicativo desenvolvido utilizando Firebase
+//---------------------------------------------------
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
